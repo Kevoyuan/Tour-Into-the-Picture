4f84828b468453b458b2b83d8634ac84c46c14d4
Kevoyuan <kevinsyuan@gmail.com>
2022-06-30 23:09:24 +0200

    updatable radial line

--- spidery_mesh.m
+++ spidery_mesh.m
@@ -15,8 +15,8 @@ hold on
 
 
 % Draw vanishing point
-
 % initial VanishingPoint(middle of the Picture)
+
 vb= .5 * (m);
 va =.5 * (n);
 
@@ -25,7 +25,8 @@ pos_VanishingPoint = [va vb];
 roi_VanishingPoint = images.roi.Point(gca,'Position',pos_VanishingPoint,"Color",'k',"LineWidth",5);
 hold on
 % Draw inner rectangle
-555555
+
+
 % [x y w h]= x and y elements determine the location (top-left) and the w and h elements determine the size
 pos_InnerRectangle = [va-250,vb-250,500,500];
 roi_InnerRectangle = drawrectangle('Color','k','FaceAlpha', 0, ...
@@ -45,32 +46,29 @@ roi_InnerRectangle.Position = pos_InnerRectangle
 % roi_line2 = drawline('Position',[r_top_right;pos_VanishingPoint],'Color','r');
 % roi_line3 = drawline('Position',[r_bottom_left;pos_VanishingPoint],'Color','r');
 % roi_line4 = drawline('Position',[r_botton_right;pos_VanishingPoint],'Color','r');
-
-
-%% Get Positions of vanishing point & inner rectangle
+% Get Positions of vanishing point & inner rectangle
 % 1. position of vanishing point
 
-
-% addlistener(roi_VanishingPoint,'MovingROI',@(src, evt) roiChange(src,evt,'Updated_VanishingPoint'));
+addlistener(roi_VanishingPoint,'MovingROI',@(src, evt) roiChange(src,evt,'Updated_VanishingPoint'));
 %% 
 % 2. position of inner rectangle
 
-% addlistener(roi_InnerRectangle,'MovingROI',@(src, evt) roiChange(src,evt,'Updated_InnerRectangle'));
+addlistener(roi_InnerRectangle,'MovingROI',@(src, evt) roiChange(src,evt,'Updated_InnerRectangle'));
 %% 
 % updated vertices of inner rectangle
 
 % Updated_InnerRectangle = 'Updated_InnerRectangle.mat';
 % save(Updated_InnerRectangle)
 % load Updated_InnerRectangle.mat
-
-
+% 
+% 
 % updated_top_left = [Updated_InnerRectangle(1), Updated_InnerRectangle(2)]
 % updated_top_right = [Updated_InnerRectangle(1) + Updated_InnerRectangle(3), Updated_InnerRectangle(2)]
 % updated_bottom_left = [Updated_InnerRectangle(1), Updated_InnerRectangle(2)+Updated_InnerRectangle(4)]
 % updated_botton_right = [Updated_InnerRectangle(1)+Updated_InnerRectangle(3), Updated_InnerRectangle(2)+Updated_InnerRectangle(4)]
-
-
-roi_InnerRectangle.Position
+% 
+% 
+% roi_InnerRectangle.Position
 
 updated_top_left = [roi_InnerRectangle.Position(1), roi_InnerRectangle.Position(2)]
 updated_top_right = [roi_InnerRectangle.Position(1) + roi_InnerRectangle.Position(3), roi_InnerRectangle.Position(2)]
@@ -92,6 +90,8 @@ V = roi_VanishingPoint.Position;
 % 
 % radialline_right(V,updated_top_right,Image2);
 % radialline_right(V,updated_botton_right,Image2);
+
+l1 = addlistener(roi_VanishingPoint,'MovingROI',@(src, evt) radialline_right_test(src,evt,updated_botton_right,Image2));
 
 
 %%
@@ -131,12 +131,19 @@ function roi = roiChange(~,evt,roi)
 
 end
 
-function radialline_right_test(CenterPoint, ThroPoint, img)
+%% 
+% live updatable radial line
 
+
+function radialline_right_test(src, evt,ThroPoint,img)
+
+l = findall(gcf,'Type', 'Line'); 
+
+delete(l); 
+% get current vanishing point position
+C = evt.CurrentPosition;
 % coefficients = polyfit([x1 x2], [y1 y2], 1);
-CenterPoint
-
-coefficients = polyfit([CenterPoint(1) ThroPoint(1)], [CenterPoint(2) ThroPoint(2)], 1);
+coefficients = polyfit([C(1) ThroPoint(1)], [C(2) ThroPoint(2)], 1);
 a = coefficients (1);
 b = coefficients (2);
 
@@ -144,5 +151,6 @@ aLine = [a,-1,b];
 
 points = lineToBorderPoints(aLine,size(img));
 
-line([points(3),CenterPoint(1)],[points(4),CenterPoint(2)],'Color', 'r', 'LineWidth', 2);
+line([points(3),C(1)],[points(4),C(2)],'Color', 'r', 'LineWidth', 2);
+drawnow
 end
