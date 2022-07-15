5f1b386d8e180b413291dd236917a30fc79eca33
ge23bak <ge23bak@mytum.de>
2022-07-16 03:46:28 +0800

    add hide ；

--- get_polygon_function.m
+++ get_polygon_function.m
@@ -3,17 +3,17 @@ function polygon_function = get_polygon_function(P1,P2,vertical_surface)
 %假设垂直方程输入的形式是 （syms） f = Ax + By +Cz +D ；
 syms x y z D pf 
 % the normal vector of the basic background
- n_vsurface = equationsToMatrix(vertical_surface,[x y z])
+ n_vsurface = equationsToMatrix(vertical_surface,[x y z]);
 %应该得到[A B C] 行向量
 % the normal vector of the polygon
- n_psurface = cross(n_vsurface',P2-P1)
+ n_psurface = cross(n_vsurface',P2-P1);
 % assume the function of the polygon in general form
-pf = n_psurface(1)*x + n_psurface(2)*y + n_psurface(3)*z + D
+pf = n_psurface(1)*x + n_psurface(2)*y + n_psurface(3)*z + D;
 %P1 is on the polygon surface
-pf_P1 =subs(pf,[x,y,z],P1')
-sol_D = vpasolve(pf_P1)
+pf_P1 =subs(pf,[x,y,z],P1');
+sol_D = vpasolve(pf_P1);
 %get the polygon function
-polygon_function = subs(pf,D,sol_D)
+polygon_function = subs(pf,D,sol_D);
 end
 
 
