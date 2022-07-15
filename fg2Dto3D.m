0fa2bb52b7736e78cd73f569190437e24702077d
ge23bak <ge23bak@mytum.de>
2022-07-16 01:03:50 +0800

    fg main funtion

--- fg2Dto3D.m
+++ fg2Dto3D.m
@@ -1,16 +1,21 @@
-function [Mfg3D poly_f Mfg2D] = fg2Dto3D(n,gray_image,new_TwelfPoints_vp,Twelfpoints_3D,k)
-% gary_image : expanded image
-% n :the number of the foregrounds object
-Mfg2D = zeros(2,4*n);
+function [Mfg3D poly_f Mfg2D] = fg2Dto3D(n,origin_image,new_TwelfPoints_vp,Twelfpoints_3D,Mfg2D)
 Mfg3D = zeros(3,4*n);
 attached_bg = cell(1,n); 
-imgsize = size(gray_image);
 focal_length = 1
 syms poly_f 
-% f is the polygon function
-% the first loop n is the number of the foregroundobjects
+% poly_f is the polygon function
 for i = 1 :n
-    %{
+    attached_bg{i} = verticalbg_of_fg(Mfg2D(:,4*i-3:4*i),new_TwelfPoints_vp)
+    [Mfg3D(:,4*i-3:4*i)  poly_f(i)]= get_fg3D_parent(new_TwelfPoints_vp,Twelfpoints_3D,Mfg2D(:,4*i-3:4*i),attached_bg{i})
+    outW = max(Mfg2D(2,4*i-3:4*i))-min(Mfg2D(2,4*i-3:4*i));
+    outH = max(Mfg2D(1,4*i-3:4*i))-min(Mfg2D(1,4*i-3:4*i));
+    fg2Dimage = Perspective_transform(origin_image, Mfg2D(:,4*i)', Mfg2D(:,4*i-1)', Mfg2D(:,4*i-3)', Mfg2D(:,4*i-2)', round(outH), round(outW))
+    imwrite(fg2Dimage,sprintf('fg%d.jpg',i));
+
+end
+
+end
+ %{
     % GUI infomation
     fig = uifigure;
     message = {sprintf('please select the foregroundobject %d',i)};
@@ -18,11 +23,7 @@ for i = 1 :n
     'Icon','info');
     % diplay the image and the user begin to choose the polygon
     %}
-    figure
-    imshow(gray_image)
-    Mfg2D(:,4*i-3:4*i) = get_fg2D(gray_image)
-    
-    attached_bg{i} = verticalbg_of_fg(Mfg2D(:,4*i-3:4*i),new_TwelfPoints_vp);
+
     %{
     if strcmp(attached_bg{i},error)
         fig = uifigure;
@@ -31,16 +32,3 @@ for i = 1 :n
         'Icon','warning');
     else
     %}
-        
-        [Mfg3D(:,4*i-3:4*i)  poly_f(i)]= get_fg3D_parent(new_TwelfPoints_vp,Twelfpoints_3D,Mfg2D(:,4*i-3:4*i),attached_bg{i},k)
-        outW = (max(Mfg3D(2,4*i-3:4*i))-min(Mfg3D(2,4*i-3:4*i)))/1000;
-        outH = (max(Mfg3D(1,4*i-3:4*i))-min(Mfg3D(1,4*i-3:4*i)))/1000;
-        fg2Dimage = Perspective_transform(gray_image, Mfg2D(:,4*i)', Mfg2D(:,4*i-1)', Mfg2D(:,4*i-3)', Mfg2D(:,4*i-2)', round(outH), round(outW))
-        
-        imwrite(fg2Dimage,sprintf('fg%d.jpg',i));
-        
-    % end
-end
-
-end
-
