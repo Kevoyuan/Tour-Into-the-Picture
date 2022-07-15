36842f8ef99d091a15fffb2f68e36c96182b995a
ge23bak <ge23bak@mytum.de>
2022-07-15 15:20:54 +0800

    point mapping

--- get_fg3D_parent.m
+++ get_fg3D_parent.m
@@ -1,57 +1,198 @@
-function [fg3D  poly_f]= get_fg3D_parent(new_TwelfPoints_vp,focal_length,fg2D,atteched_bg,imgsize,d,k,Twelfpoints_3D)
-% alle is the value in the expanded image
-% vx vy vanishing point 2D value
-% P1 point 1 2D value
-% f focal length
+function [fg3D  poly_f]= get_fg3D_parent(TwelfPoints_2D,TwelfPoints_3D,fg2D,atteched_bg,k)
+% alle 2D is the value in the expanded image
 % k the magnification factor >1
-% d = (h*f/a) -f 
 syms x y z poly_f fv
-vanish_y = new_TwelfPoints_vp(2,13);
-% vx = new_TwelfPoints_vp(1,13);
-P1 = new_TwelfPoints_vp(:,1);
-P3 = new_TwelfPoints_vp(:,3);
-P2 = new_TwelfPoints_vp(:,2);
-P6 = new_TwelfPoints_vp(:,6);
-P1_3D = Twelfpoints_3D(:,1);
-P8_3D = Twelfpoints_3D(:,8);
+P1_2D = TwelfPoints_2D(:,1);
+P2_2D = TwelfPoints_2D(:,2);
+P3_2D = TwelfPoints_2D(:,3);
+P4_2D = TwelfPoints_2D(:,4);
+P5_2D = TwelfPoints_2D(:,5);
+P6_2D = TwelfPoints_2D(:,6);
+P7_2D = TwelfPoints_2D(:,7);
+P8_2D = TwelfPoints_2D(:,8);
+P9_2D = TwelfPoints_2D(:,9);
+P10_2D = TwelfPoints_2D(:,10);
+P11_2D = TwelfPoints_2D(:,11);
+P12_2D = TwelfPoints_2D(:,12);
+P1_3D = TwelfPoints_3D(:,1);
+P4_3D = TwelfPoints_3D(:,4);
+P8_3D = TwelfPoints_3D(:,8);
+P9_3D = TwelfPoints_3D(:,9);
+P11_3D = TwelfPoints_3D(:,11);
+P6_3D = TwelfPoints_3D(:,6);
+
 if strcmp(atteched_bg,'floor')
-    fg3D(2,[1,2]) =P1_3D(2);
-    fg3D(2,[3,4]) = k * fg2D(2,[3,4])-fg2D(2,[2,1])+P1_3D(2);% 等高放大
-    %fg2D(2,[3,4])确认形式
-    fg3D(3,[1,4]) =  d - ((imgsize(1)-fg2D(2,1))*focal_length/(fg2D(2,1)-vanish_y))+P1_3D(3);
-    fg3D(3,[2,3]) = d - ((imgsize(1)-fg2D(2,2))*focal_length/(fg2D(2,2)-vanish_y))+P1_3D(3);% 相似三角形求景深
-    fg3D(1,[1,2,3,4]) = k * (fg2D(1,[1,2,3,4])-P1(1))+P1_3D(1);% 与1点的x 距离成比例放大
+    fg3D(:,1) = transfor_floor2Dto3D(fg2D(:,1),P1_2D,P2_2D,P3_2D,P4_2D,P4_3D,P1_3D);
+    fg3D(:,2) = transfor_floor2Dto3D(fg2D(:,2),P1_2D,P2_2D,P3_2D,P4_2D,P4_3D,P1_3D);
+    fg3D(1:2,4) = k * (fg2D(:,4)-fg2D(:,1))+fg3D(1:2,1);  %图形等比放大
+    fg3D(3,4) = fg3D(3,1);
+    fg3D(1:2,3) = k * (fg2D(:,3)-fg2D(:,4))+fg3D(1:2,4); 
+    fg3D(3,3) = fg3D(3,2);
     fv = 0*x + 1*y +0*z -P1_3D(2)
     poly_f= get_polygon_function(fg3D(:,1),fg3D(:,2),fv);
 elseif strcmp(atteched_bg,'ceiling')
-    fg3D(2,[3,4]) =P8_3D(2);
-    fg3D(2,[1,2]) =P8_3D(2) - k * fg2D(2,[1,2])-fg2D(2,[4,3]);% 等高放大
-    %fg2D(2,[3,4])确认形式
-    fg3D(3,[1,4]) = d -( fg2D(2,4)*focal_length /(vanish_y-fg2D(2,4)))+P1_3D(3);
-    fg3D(3,[2,3]) = d -( fg2D(2,3)*focal_length /(vanish_y-fg2D(2,3)))+P1_3D(3);% 相似三角形求景深
-    fg3D(1,[1,2,3,4]) = k * (fg2D(1,[1,2,3,4])-P1(1))+P1_3D(1);% 与1点的x 距离成比例放大
-    fv = 0 *x + 1*y +0*z -P8_3D(2);
-    poly_f = get_polygon_function(fg3D(:,3),fg3D(:,4),fv);
+    fg3D(:,4) = transfor_ceiling2Dto3D(fg2D(:,4),P9_2D,P10_2D,P7_2D,P8_2D,P8_3D,P9_3D);
+    fg3D(:,3) = transfor_ceiling2Dto3D(fg2D(:,3),P9_2D,P10_2D,P7_2D,P8_2D,P8_3D,P9_3D);
+    fg3D(1:2,1) = k * (fg2D(:,1)-fg2D(:,4))+fg3D(1:2,4);  %图形等比放大
+    fg3D(3,1) = fg3D(3,4);
+    fg3D(1:2,2) = k * (fg2D(:,2)-fg2D(:,1))+fg3D(1:2,1); 
+    fg3D(3,2) = fg3D(3,3);
+    fv = 0*x + 1*y + 0*z -P8_3D(2);
+    poly_f= get_polygon_function(fg3D(:,3),fg3D(:,4),fv);
 elseif strcmp(atteched_bg,'leftwall')
-    fg3D(1,[2,3]) =P8_3D(1);
-    fg3D(1,[1,4]) = P8_3D(1) - k * fg2D(1,[2,3])-fg2D(1,[1,4]);
-    yc2 = (P6(2) - P2(2))/(P6(1) - P2(1)) * fg2D(1,2) - ((P6(2) - P2(2))/(P6(1) - P2(1)))*P2(1) + P2(2);
-    yc3 = (P6(2) - P2(2))/(P6(1) - P2(1)) * fg2D(1,3) - ((P6(2) - P2(2))/(P6(1) - P2(1)))*P2(1) + P2(2);
-    fg3D(3,[1,2]) =  d - (imgsize(1)-yc2)*focal_length/(yc2-vanish_y)+P1_3D(3);
-    fg3D(3,[3,4]) = d - (imgsize(1)-yc3)*focal_length/(yc3-vanish_y)+P1_3D(3);
-    fg3D(2,1) = -k * (fg2D(2,1)-yc2)+P1_3D(3);
-    fg3D(2,[2,3,4]) = fg3D(2,1) + k * (fg2D(2,[2,3,4])-fg2D(2,1));
-    fv = 1 *x + 0*y +0*z -P8_3D(1);
-    poly_f= get_polygon_function(fg3D(:,2),fg3D(:,3),fv );
-elseif strcmp(atteched_bg,'rightwall')
-    fg3D(1,[1,4]) =P1_3D(1);
-    fg3D(1,[2,3]) =k * fg2D(1,[2,3])-fg2D(2,[1,4])+P1_3D(1);
-    yc1 = (P1(2) - P3(2))/(P1(1) - P3(1)) * fg2D(1,1) - ((P1(2) - P3(2))/(P1(1) - P3(1)))*P3(1) + P3(2);
-    yc4 = (P1(2) - P3(2))/(P1(1) - P3(1)) * fg2D(1,4) - ((P1(2) - P3(2))/(P1(1) - P3(1)))*P3(1) + P3(2);
-    fg3D(3,[1,2]) = d - (imgsize(1)-yc1)*focal_length/(yc1-vanish_y)+P1_3D(3);
-    fg3D(3,[3,4]) = d - (imgsize(1)-yc4)*focal_length/(yc4-vanish_y)+P1_3D(3);
-    fg3D(2,1) = -k * (fg2D(2,1)-yc1)+P1_3D(2);
-    fg3D(2,[2,3,4]) = fg3D(2,1) + k * (fg2D(2,[2,3,4])-fg2D(2,1));
+    fg3D(:,1) = transfor_left2Dto3D(fg2D(:,1),P11_2D,P7_2D,P5_2D,P1_2D,P1_3D,P11_3D);
+    fg3D(:,4) = transfor_left2Dto3D(fg2D(:,4),P11_2D,P7_2D,P5_2D,P1_2D,P1_3D,P11_3D);
+    fg3D(1:2,2) = k * (fg2D(:,2)-fg2D(:,1))+fg3D(1:2,1);
+    fg3D(3,2) = fg3D(3,1);
+    fg3D(1:2,3) = k * (fg2D(:,3)-fg2D(:,2))+fg3D(1:2,2); 
+    fg3D(3,3) = fg3D(3,4);
     fv = 1 *x + 0*y +0*z -P1_3D(1);
     poly_f= get_polygon_function(fg3D(:,1),fg3D(:,4),fv );
+elseif strcmp(atteched_bg,'rightwall')
+    fg3D(:,2) = transfor_right2Dto3D(fg2D(:,2),P8_2D,P12_2D,P2_2D,P4_2D,P8_3D,P6_3D);
+    fg3D(:,3) = transfor_right2Dto3D(fg2D(:,3),P8_2D,P12_2D,P2_2D,P4_2D,P8_3D,P6_3D);
+    fg3D(1:2,1) = k * (fg2D(:,1)-fg2D(:,2))+fg3D(1:2,2);
+    fg3D(3,1) = fg3D(3,2);
+    fg3D(1:2,4) = k * (fg2D(:,4)-fg2D(:,1))+fg3D(1:2,1); 
+    fg3D(3,4) = fg3D(3,3);
+    fv = 1 *x + 0*y +0*z -P8_3D(1);
+    poly_f= get_polygon_function(fg3D(:,1),fg3D(:,4),fv );
+end
+
+end
+
+%% floor
+function fg3D = transfor_floor2Dto3D(fg2D,pLT,pRT,pLB,pRB,P4,P1)
+%输出3D坐标， 输入2D图中前景的2维坐标，floor四边形四个顶点坐标，深度，宽度
+%koordinate_2d:[x,y]'
+    
+    vector_Or01 = pRT - pLT; % 向量Or01 = r01 - O
+    vector_Or10 = pLB - pLT; 
+    vector_Or11 = pRB - pLT;
+    
+    A = [vector_Or10 , vector_Or01];
+    B = vector_Or11;
+    a_solution = A\B;
+    a0 = a_solution(1);
+    a1 = a_solution(2);
+  
+    vector_Or = fg2D - pLT;
+    
+    C = vector_Or;
+    y_solution = A\C;
+    y0 = y_solution(1);
+    y1 = y_solution(2);
+    
+    Denominator = a0*a1+a1*(a1-1)*y0+a0*(a0-1)*y1;            
+    x0 = a1*(a0+a1-1)*y0/Denominator;
+    x1 = a0*(a0+a1-1)*y1/Denominator;
+
+    width = P4(1)-P1(1);
+    tief = P1(3) - P4(3);
+    Z= - round(tief*x0)+P1(3)
+    Y=P1(2);
+    X = round(x1*width)+P1(1);
+    fg3D = [X;Y;Z];
+end
+
+%% ceiling
+function fg3D = transfor_ceiling2Dto3D(fg2D,pLT,pRT,pLB,pRB,P8,P9)
+%输出3D坐标， 输入2D图中前景的2维坐标，ceiling四边形四个顶点坐标，深度，宽度
+%koordinate_2d:[x,y]'
+    
+    vector_Or01 = pRT - pLT; % 向量Or01 = r01 - O
+    vector_Or10 = pLB - pLT; 
+    vector_Or11 = pRB - pLT;
+    
+    A = [vector_Or10 , vector_Or01];
+    B = vector_Or11;
+    a_solution = A\B;
+    a0 = a_solution(1);
+    a1 = a_solution(2);
+  
+    vector_Or = fg2D - pLT;
+    
+    C = vector_Or;
+    y_solution = A\C;
+    y0 = y_solution(1);
+    y1 = y_solution(2);
+    
+    Denominator = a0*a1+a1*(a1-1)*y0+a0*(a0-1)*y1;            
+    x0 = a1*(a0+a1-1)*y0/Denominator;
+    x1 = a0*(a0+a1-1)*y1/Denominator;
+
+    width = P8(1)-P9(1);
+    tief = P8(3) - P9(3);
+    Z=  round(tief*x0)+P9(3)
+    Y=P1(2);
+    X = round(x1*width)+P9(1);
+    fg3D = [X;Y;Z];
+end
+
+%% leftwall
+function fg3D = transfor_left2Dto3D(fg2D,pLT,pRT,pLB,pRB,P1,P11)
+%输出3D坐标， 输入2D图中前景的2维坐标，leftwall四边形四个顶点坐标，深度，宽度
+%koordinate_2d:[x,y]'
+    
+    vector_Or01 = pRT - pLT; % 向量Or01 = r01 - O
+    vector_Or10 = pLB - pLT; 
+    vector_Or11 = pRB - pLT;
+    
+    A = [vector_Or10 , vector_Or01];
+    B = vector_Or11;
+    a_solution = A\B;
+    a0 = a_solution(1);
+    a1 = a_solution(2);
+  
+    vector_Or = fg2D - pLT;
+    
+    C = vector_Or;
+    y_solution = A\C;
+    y0 = y_solution(1);
+    y1 = y_solution(2);
+    
+    Denominator = a0*a1+a1*(a1-1)*y0+a0*(a0-1)*y1;            
+    x0 = a1*(a0+a1-1)*y0/Denominator;
+    x1 = a0*(a0+a1-1)*y1/Denominator;
+
+    height = P1(2)-P11(2);
+    tief = P1(3) - P11(3);
+    Y=  round(height*x0)+P11(2)
+    X=P1(1);
+    Z = round(x1*tief)+P11(3);
+    fg3D = [X;Y;Z];
+end
+
+%% Rightwall
+function fg3D = transfor_right2Dto3D(fg2D,pLT,pRT,pLB,pRB,P8,P6)
+%输出3D坐标， 输入2D图中前景的2维坐标，rightwall四边形四个顶点坐标，深度，宽度
+%koordinate_2d:[x,y]'
+    
+    vector_Or01 = pRT - pLT; % 向量Or01 = r01 - O
+    vector_Or10 = pLB - pLT; 
+    vector_Or11 = pRB - pLT;
+    
+    A = [vector_Or10 , vector_Or01];
+    B = vector_Or11;
+    a_solution = A\B;
+    a0 = a_solution(1);
+    a1 = a_solution(2);
+  
+    vector_Or = fg2D - pLT;
+    
+    C = vector_Or;
+    y_solution = A\C;
+    y0 = y_solution(1);
+    y1 = y_solution(2);
+    
+    Denominator = a0*a1+a1*(a1-1)*y0+a0*(a0-1)*y1;            
+    x0 = a1*(a0+a1-1)*y0/Denominator;
+    x1 = a0*(a0+a1-1)*y1/Denominator;
+
+    height = P6(2)-P8(2);
+    tief = P6(3) - P8(3);
+    Y=  round(height*x0)+P8(2)
+    X=P8(1);
+    Z = -round(x1*tief)+P8(3);
+    fg3D = [X;Y;Z];
 end
