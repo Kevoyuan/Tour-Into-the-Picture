1b364eb0201c718939c35084a786bd6d55ac7723
ge23bak <ge23bak@mytum.de>
2022-07-16 19:16:56 +0800

    creat a rectangle according to the points the user have selected

---
+++ find_rectangular.m
@@ -0,0 +1,14 @@
+function fg2D = find_rectangular(points,n)
+
+fg2D = zeros(2,n*4)
+for i =1 :n
+    fg = points{n};
+    xmin = min(fg(1,:));
+    xmax = max(fg(1,:));
+    ymin = min(fg(2,:));
+    ymax = max(fg(2,:));
+    fg2D(:,4*i-3:4*i)=[xmin,xmax,xmax,xmin;ymax ymax ymin ymin];
+end
+end
+
+
