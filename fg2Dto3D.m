a6dd03480d41c5b4b34c371cca4b8b6501fab64f
ge23bak <ge23bak@mytum.de>
2022-07-17 22:06:09 +0800

    width and height reserve

--- fg2Dto3D.m
+++ fg2Dto3D.m
@@ -6,8 +6,8 @@ syms poly_f
 for i = 1 :n
     attached_bg{i} = verticalbg_of_fg(Mfg2D(:,4*i-3:4*i),new_TwelfPoints_vp);
     [Mfg3D(:,4*i-3:4*i)  poly_f(i)]= get_fg3D_parent(new_TwelfPoints_vp,Twelfpoints_3D,Mfg2D(:,4*i-3:4*i),attached_bg{i});
-    outW = max(Mfg2D(2,4*i-3:4*i))-min(Mfg2D(2,4*i-3:4*i));
-    outH = max(Mfg2D(1,4*i-3:4*i))-min(Mfg2D(1,4*i-3:4*i));
+    outH = max(Mfg2D(2,4*i-3:4*i))-min(Mfg2D(2,4*i-3:4*i));
+    outW = max(Mfg2D(1,4*i-3:4*i))-min(Mfg2D(1,4*i-3:4*i));
     fg2Dimage = Perspective_transform(origin_image, Mfg2D(:,4*i)', Mfg2D(:,4*i-1)', Mfg2D(:,4*i-3)', Mfg2D(:,4*i-2)', round(outH), round(outW));
     fg2Dimage = fliplr(fg2Dimage);
     imwrite(fg2Dimage,sprintf('fg%d.jpg',i));
