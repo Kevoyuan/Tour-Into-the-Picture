94222fe85bedc521a14be4b3b491115cc091127c
zhang <zhangxd1995@gmail.com>
2022-07-18 13:40:55 +0200

    keyboard

--- keypress.m
+++ keypress.m
@@ -1,14 +1,14 @@
 function keypress(ax,key)
     if strcmp(key,'leftarrow')
-     camorbit(ax,10,0,'camera')   
+     camorbit(ax,5,0,'camera')   
     end
     if strcmp(key,'rightarrow')
-     camorbit(ax,-10,0,'camera')   
+     camorbit(ax,-5,0,'camera')   
     end
     if strcmp(key,'uparrow')
-     camorbit(ax,0,10,'camera')   
+     camorbit(ax,0,5,'camera')   
     end
     if strcmp(key,'downarrow')
-     camorbit(ax,0,-10,'camera')   
+     camorbit(ax,0,-5,'camera')   
     end
 end
