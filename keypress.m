bfadf34fba9814a1d001d5a09912b6d01bf17ee6
zhang <zhangxd1995@gmail.com>
2022-07-18 23:33:52 +0200

    gui improve

--- keypress.m
+++ keypress.m
@@ -1,18 +1,18 @@
 function camera_position = keypress(ax,key)
     if strcmp(key,'leftarrow')
-     camorbit(ax,5,0,'camera')
+     camorbit(ax,2,0,'camera')
      camera_position = campos;
     end
     if strcmp(key,'rightarrow')
-     camorbit(ax,-5,0,'camera')
+     camorbit(ax,-2,0,'camera')
      camera_position = campos;
     end
     if strcmp(key,'uparrow')
-     camorbit(ax,0,5,'camera')
+     camorbit(ax,0,2,'camera')
      camera_position = campos;
     end
     if strcmp(key,'downarrow')
-     camorbit(ax,0,-5,'camera')
+     camorbit(ax,0,-2,'camera')
      camera_position = campos;
     end
 end
