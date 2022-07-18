6f3d5165ba73c3a4d7a37701ee845fe0f6403656
zhang <zhangxd1995@gmail.com>
2022-07-18 12:39:07 +0200

    adapt anaimation

--- start_animation.m
+++ start_animation.m
@@ -1,44 +1,51 @@
-function start_animation(ax,VanishingPoint_3D)
-    for z = 100:5:200
-        campos(ax,[-100,0,z])
+function start_animation(ax,VanishingPoint_3D,TwelfPoints_3D)
+    center = [(TwelfPoints_3D(1,1)+TwelfPoints_3D(1,2))/2,(TwelfPoints_3D(2,1)+TwelfPoints_3D(2,7))/2];
+    x_bound = abs((TwelfPoints_3D(1,1)-TwelfPoints_3D(1,2))/2)*0.75;
+    y_bound = abs((TwelfPoints_3D(2,7)-TwelfPoints_3D(2,1))/2)*0.75;
+    z_lbound = min(TwelfPoints_3D(3,3),TwelfPoints_3D(3,5))/3;
+    z_rbound = VanishingPoint_3D(3)*0.5;
+    
+    for z = z_lbound:5:z_rbound
+    campos(ax,[center(1),center(2),z])
+    drawnow
+    pause(.1)
+    end
+
+    for y = 0:5:y_bound
+        campos(ax,[center(1),center(2)-y,z_rbound])
+        drawnow
+        pause(.1)
+    end
+    
+    for y = y_bound:-5:0
+        campos(ax,[center(1),center(2)-y,z_rbound])
         drawnow
         pause(.1)
     end
 
-    for y = 0:5:150
-        campos(ax,[-100,-y,200])
-        drawnow
-        pause(.1)
-    end
-    for y = 150:-5:0
-        campos(ax,[-100,-y,200])
+    for x = 0:5:x_bound
+        campos(ax,[center(1)-x,center(2),z_rbound])
+        camtarget(ax,[center(1)-x,center(2),VanishingPoint_3D(3)])
         drawnow
         pause(.1)
     end
 
-    for x = 0:5:100
-        campos(ax,[-100-x,0,200])
-        camtarget(ax,[-100-x,0,VanishingPoint_3D(3)])
+    for x = x_bound:-5:-x_bound
+        campos(ax,[center(1)-x,center(2),z_rbound])
+        camtarget(ax,[center(1)-x,center(2),VanishingPoint_3D(3)])
         drawnow
         pause(.1)
     end
 
-    for x = 100:-5:-100
-        campos(ax,[-100-x,0,200])
-        camtarget(ax,[-100-x,0,VanishingPoint_3D(3)])
+    for x = -x_bound:5:0
+        campos(ax,[center(1)-x,center(2),z_rbound])
+        camtarget(ax,[center(1)-x,center(2),VanishingPoint_3D(3)])
         drawnow
         pause(.1)
     end
 
-    for x = -100:5:0
-        campos(ax,[-100-x,0,200])
-        camtarget(ax,[-100-x,0,VanishingPoint_3D(3)])
-        drawnow
-        pause(.1)
-    end
-
-    for z = 200:-5:100
-        campos(ax,[-100,0,z])
+    for z = z_rbound:-5:z_lbound
+        campos(ax,[center(1),center(2),z])
         drawnow
         pause(.1)
     end
